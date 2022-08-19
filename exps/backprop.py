c0a21a24c5c0da4c4e10c6b4e47a45a61db43c07
Yash Mehta <yashsmehta95@gmail.com>
2022-08-19 00:26:23 -0400

    SGD on toy network

--- exps/backprop.py
+++ exps/backprop.py
@@ -1,4 +1,3 @@
-import time
 import jax
 from jax.tree_util import Partial
 import jax.numpy as jnp
@@ -14,25 +13,33 @@ def mse_loss(layer, y, state, parameters):
 
 def main():
     dataset = ds.AndDataSet()
-    key = jax.random.PRNGKey(round(time.time()))
+    key = jax.random.PRNGKey(0)
     x, y = dataset.get_noisy_samples(num=1, key=key, sigma=0.0)
-    print("x:", x)
-    print("y:", y)
     lr = 0.2
+    max_iter = 5
     edges = [(0, 0), (1, 0)]
 
     layer = px.layers.SparseLayer(
-        2, 1, edges, px.kernels.edges.FixedWeight(), px.kernels.nodes.SumNonlinear()
+        2,
+        1,
+        edges,
+        px.kernels.edges.FixedWeight(),
+        px.kernels.nodes.SumNonlinear(),
     )
     state = layer.init_state()
     parameters = layer.init_parameters()
 
-    state.input_nodes.rate = x
     state = layer.update_state(state, parameters)
-    # state = layer.update_state(state, parameters)
     loss = Partial((mse_loss), layer)
-    grads = jax.grad(loss, argnums=2)(y, state, parameters)
-    parameters.edges.weight += lr * grads.edges.weight
+
+    for _ in range(max_iter):
+        key, _ = jax.random.split(key)
+        x, y = dataset.get_noisy_samples(num=1, key=key, sigma=0.0)
+        state.input_nodes.rate = x
+        grads = jax.grad(loss, argnums=2)(y, state, parameters)
+        parameters.edges.weight += lr * grads.edges.weight
+
+    state = layer.update_state(state, parameters)
     print("edge parameters:", parameters.edges.weight)
     print("edge state:", state.edges.signal)
     print("prediction: ", state.output_nodes.rate)
