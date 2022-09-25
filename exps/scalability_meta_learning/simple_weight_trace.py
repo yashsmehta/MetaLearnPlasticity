63db5aed7a5de2151d8ec6faab91594844c18508
Yash Mehta <yashsmehta95@gmail.com>
2022-09-25 18:24:27 -0400

    Added jax.jit and logging

--- exps/scalability_meta_learning/simple_weight_trace.py
+++ exps/scalability_meta_learning/simple_weight_trace.py
@@ -1,12 +1,13 @@
 import jax
 import jax.numpy as jnp
 import optax
-
+import time
+import pandas as pd
 
 def generate_gaussian(key, shape, scale=0.1):
     return scale * jax.random.normal(key, (shape))
 
-
+@jax.jit
 def generate_weight_trajectory(x, weights, A):
     weight_trajectory = []
 
@@ -16,7 +17,7 @@ def generate_weight_trajectory(x, weights, A):
 
     return weight_trajectory
 
-
+@jax.jit
 def trajectory_loss(x, weights, A, weight_trajectory):
     loss = 0
 
@@ -35,9 +36,9 @@ def update_weights(x, weights, A):
 
 def main():
     key = jax.random.PRNGKey(0)
-    meta_epochs = 1
-    num_trajectories = 5
-    length = 5
+    meta_epochs = 50 
+    num_trajectories = 100
+    length = 10 
     m, n = 5, 1
     teacher_weights = generate_gaussian(key, (m, n), scale=1 / (m + n))
     student_weights = generate_gaussian(key, (m, n), scale=1 / (m + n))
@@ -50,8 +51,14 @@ def main():
     optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(A_student)
 
+    expdata = {'A_0':[], 'A_1':[], 'loss':[]} 
+    expdata['epoch'] = jnp.arange(meta_epochs)
+
     for epoch in range(meta_epochs):
+        start_time = time.time()
         key = jax.random.PRNGKey(0)
+        expdata['loss'].append(0.)
+
         for _ in range(num_trajectories):
             key, _ = jax.random.split(key)
             x = generate_gaussian(key, (length, m), scale=0.1)
@@ -62,8 +69,8 @@ def main():
             loss_t, grads = jax.value_and_grad(trajectory_loss, argnums=2)(
                 x, student_weights, A_student, weight_trajectory
             )
-            print("loss: ", loss_t)
-            print("grads A: ", grads)
+            expdata['loss'][-1]+=loss_t
+
             updates, opt_state = optimizer.update(
                 grads,
                 opt_state,
@@ -71,7 +78,16 @@ def main():
             )
             A_student = optax.apply_updates(A_student, updates)
 
-    print("A final:", A_student)
+        expdata['A_0'].append(A_student[0])
+        expdata['A_1'].append(A_student[1])
+
+        print("A student:", A_student)
+        print("loss (all trajectories):", expdata['loss'][-1])
+        print("epoch {} time: {}s".format(epoch, time.time() - start_time))
+        print()
+
+    df = pd.DataFrame(expdata)
+    df.to_csv('exps/scalability_meta_learning/expdata.csv')
 
 
 if __name__ == "__main__":
