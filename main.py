ba94f208b519147d92f6a93d42a97913cb84ec54
Yash Mehta <yashsmehta95@gmail.com>
2022-12-09 18:53:47 -0500

    ✨ Added efficient implementation with vmap & lax.scan

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import optax
 import time
 import numpy as np
 
-import plasticity
+import network
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
@@ -20,54 +20,20 @@ def generate_gaussian(key, shape, scale=0.1):
     return scale * jax.random.normal(key, shape)
 
 
-class Network:
-    """
-    class to bundle functions to generate activity trajectory, given the
-    initial weights and input sequence
-    """
-
-    def __init__(self, non_linear=True):
-        self.non_linear = non_linear
-        self.update_weights = jit(plasticity.volterra_update_weights)
-
-    @jit
-    def generate_trajectory(
-        self, input_sequence, initial_weights, volterra_coefficients
-    ):
-        """
-        generate a single trajectory given an input sequence, initial weights
-        and the "meta" plasticity coefficients
-        """
-
-        def step(weights, inputs):
-            return self.update_weights(inputs, weights, volterra_coefficients)
-
-        final_weights, activities = jax.lax.scan(
-            step, initial_weights, input_sequence
-            )
-        return activities
-
-    @jit
-    def forward(self, inputs, weights):
-        activatation = inputs @ weights
-        if self.non_linear:
-            activatation = jax.nn.sigmoid(activatation)
-        return activatation
-
-
-@jit
 def compute_loss(student_trajectory, teacher_trajectory):
     return jnp.mean(optax.l2_loss(student_trajectory, teacher_trajectory))
 
 
+@jit
 def compute_plasticity_coefficients_loss(
-    input_sequence, initial_weights, oja_coefficients, student_coefficients
-):
+        input_sequence,
+        initial_weights,
+        oja_coefficients,
+        student_coefficients):
     """
     function will generate the teacher trajectory and student trajectory
     using corresponding coefficients and then compute the mse loss between them
     """
-    network = Network()
     teacher_trajectory = network.generate_trajectory(
         input_sequence, initial_weights, oja_coefficients
     )
@@ -80,48 +46,54 @@ def compute_plasticity_coefficients_loss(
 
 
 if __name__ == "__main__":
-    key = jax.random.PRNGKey(0)
-    num_trajec, len_trajec = 5, 500
-    epochs = 1
+    num_trajec, len_trajec = 200, 100
+    input_dim, output_dim = 100, 10
+    epochs = 50
 
-    initial_weights = generate_gaussian(key, (100, 10))
+    key = jax.random.PRNGKey(0)
+    initial_weights = generate_gaussian(key,
+        (input_dim, output_dim),
+        scale=1 / (input_dim + output_dim))
+
+    key, key2 = jax.random.split(key)
+    # (num_trajectories, length_trajectory, input_dim)
     input_data = generate_gaussian(
-        key, (num_trajec, len_trajec, 100)
-    )  # (num_trajectories, length_trajectory, input_dim)
+        key,
+        (num_trajec, len_trajec, input_dim),
+        scale=0.1)
 
     oja_coefficients = np.zeros((3, 3, 3))
     oja_coefficients[1][1][0] = 1
     oja_coefficients[0][2][1] = -1
-    student_coefficients = generate_gaussian(key, (3, 3, 3), scale=1e-4)
+    # student_coefficients = generate_gaussian(key2, (3, 3, 3), scale=1e-4)
+    student_coefficients = jnp.zeros((3,3,3))
 
     optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(student_coefficients)
 
     for _ in range(epochs):
+        loss_i = 0
         for j in range(num_trajec):
             start = time.time()
             input_sequence = input_data[j]
 
-            # loss = compute_plasticity_coefficients_loss(
-            #     input_sequence, initial_weights,
-            #     oja_coefficients, student_coefficients
-            # )
-
-            loss, grads = jax.value_and_grad(
-                compute_plasticity_coefficients_loss, argnums=3
-            )(
-                input_sequence, initial_weights,
-                oja_coefficients, student_coefficients
-            )
-
-            print("loss ", loss)
+            loss, grads = jax.value_and_grad(compute_plasticity_coefficients_loss, argnums=3)(
+                input_sequence,
+                initial_weights,
+                oja_coefficients,
+                student_coefficients)
+            loss_i += loss
 
             updates, opt_state = optimizer.update(
                 grads,
                 opt_state,
-                student_coefficients,
-            )
+                student_coefficients)
+
             student_coefficients = optax.apply_updates(
-                student_coefficients, updates
-                )
-            print("time for backprop update: ", time.time() - start)
+                student_coefficients,
+                updates)
+
+        print("loss ", loss_i)
+
+    print("oja coefficients\n", oja_coefficients)
+    print("student coefficients\n", student_coefficients)
--- network.py
+++ network.py
@@ -3,20 +3,44 @@ import jax.numpy as jnp
 from jax.lax import reshape
 from jax import jit, vmap
 
+non_linear = True
 
-def volterra_update_weights(self, inputs, weights, volterra_coefficients):
-    activation = self.forward(inputs, weights)
+def generate_trajectory(
+        input_sequence,
+        initial_weights,
+        volterra_coefficients):
+    """
+    generate a single trajectory given an input sequence, initial weights
+    and the "meta" plasticity coefficients
+    """
+
+    def step(weights, inputs):
+        return volterra_update_weights(inputs, weights, volterra_coefficients)
+
+    final_weights, activity_trajec = jax.lax.scan(
+        step, initial_weights, input_sequence
+        )
+    return activity_trajec
+
+def forward(inputs, weights):
+    activatation = inputs @ weights
+    if non_linear:
+        activatation = jax.nn.sigmoid(activatation)
+    return activatation
+
+def volterra_update_weights(inputs, weights, volterra_coefficients):
+    activation = forward(inputs, weights)
     m, n = weights.shape
     in_grid, out_grid = jnp.meshgrid(
         reshape(inputs, (m,)), reshape(activation, (n,)), indexing="ij"
     )
 
     dw = vmap(volterra_synaptic_dw, in_axes=(0, 0, 0, None))(
-        reshape(in_grid, (m * n, 1)),
-        reshape(out_grid, (m * n, 1)),
-        reshape(weights, (m * n, 1)),
-        volterra_coefficients,
-    )
+            reshape(in_grid, (m * n, 1)),
+            reshape(out_grid, (m * n, 1)),
+            reshape(weights, (m * n, 1)),
+            volterra_coefficients)
+    
     dw = reshape(dw, (m, n))
     assert (
         dw.shape == weights.shape
@@ -33,17 +57,16 @@ def volterra_synaptic_dw(pre, post, weight, volterra_coefficients):
 
 
 def get_synapse_tensor(pre, post, weight):
-    local_tensor = jnp.outer(
+    synapse_tensor = jnp.outer(
         jnp.outer(
             jnp.array([pre**0, pre**1, pre**2]),
-            jnp.array([post**0, post**1, post**2]),
-        ),
-        jnp.array([weight**0, weight**1, weight**2]),
-    )
-    local_tensor = jnp.reshape(local_tensor, (3, 3, 3))
-    return local_tensor
+            jnp.array([post**0, post**1, post**2])),
+        jnp.array([weight**0, weight**1, weight**2]))
+    
+    synapse_tensor = jnp.reshape(synapse_tensor, (3, 3, 3))
+    return synapse_tensor
 
-
+@jit
 def mlp_update_weights(self, inputs, weights, plasticity_mlp):
     act = self.forward(inputs, weights)
     n, m = weights.shape
