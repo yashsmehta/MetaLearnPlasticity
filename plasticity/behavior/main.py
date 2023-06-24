38569b34bc87fdc4f6816f0115c98af218c79b37
Yash Mehta <yashsmehta95@gmail.com>
2023-06-24 14:58:17 -0400

    resolve max length bug

--- plasticity/behavior/main.py
+++ plasticity/behavior/main.py
@@ -42,16 +42,16 @@ def convert_xs_to_tensor(nested_list):
     num_trials = num_blocks * trials_per_block
     element_dim = 2
 
-    longest_trial_length = max(
-        max(
-            [
+    trial_lengths = [
                 [len(nested_list[j][i]) for i in range(trials_per_block)]
                 for j in range(num_blocks)
             ]
-        )
-    )
 
+    longest_trial_length = np.max(np.array(trial_lengths))
+    print("xs: ", nested_list)
+    
     tensor = np.full((num_trials, longest_trial_length, element_dim), np.nan)
+    print("tensor shape : ", tensor.shape)
 
     for i in range(num_blocks):
         for j in range(trials_per_block):
@@ -65,16 +65,16 @@ def convert_xs_to_tensor(nested_list):
 if __name__ == "__main__":
     num_epochs = 1
     num_blocks, trials_per_block = 3, 5
-    reward_ratios = ((0.2, 0.8), (0.8, 0.2), (0.2, 0.8))
-    plasticity_mask = np.zeros((3, 3, 3))
-    # plasticity_mask[1][1][0] = 1
+    reward_ratios = ((0.2, 0.8), (0.2, 0.8), (0.2, 0.8))
+    plasticity_mask = np.ones((3, 3, 3))
+    plasticity_mask[1][1][0] = 1
     # assert len(reward_ratios) == num_blocks, print("length of reward ratios should be equal to number of blocks!")
 
     input_dim, output_dim = 2, 1
     key = jax.random.PRNGKey(0)
 
     fly_plasticity_coeff, plasticity_func = synapse.init_reward_volterra(init="reward")
-    insilico_plasticity_coeff, _ = synapse.init_reward_volterra(init="zeros")
+    insilico_plasticity_coeff, _ = synapse.init_reward_volterra(init="reward")
 
     key, key2 = split(key)
 
@@ -117,11 +117,18 @@ if __name__ == "__main__":
     print("decisions: \n", decisions)
     trial_lengths = jnp.sum(jnp.logical_not(jnp.isnan(decisions)), axis=1).astype(int)
 
-    outputs, _ = network.simulate_insilico_experiment(
-        winit, fly_plasticity_coeff, plasticity_func, xs, rewards, exp_rewards, trial_lengths
+    loss = losses.celoss(
+        winit,
+        insilico_plasticity_coeff,
+        plasticity_func,
+        xs,
+        rewards,
+        exp_rewards,
+        decisions,
+        trial_lengths,
+        plasticity_mask,
     )
-    print("outputs: \n", outputs)
-    exit()
+
     loss_value_and_grad = jax.value_and_grad(losses.celoss, argnums=1)
     optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(insilico_plasticity_coeff)
@@ -140,6 +147,10 @@ if __name__ == "__main__":
             trial_lengths,
             plasticity_mask,
         )
+        print("grads: \n", meta_grads)
+        print()
+        print("loss: ", loss)
+        exit()
         # check if loss is nan
         if np.isnan(loss):
             print("loss is nan!")
