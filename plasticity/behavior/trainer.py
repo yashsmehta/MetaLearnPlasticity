3e8cac7789c916440ba40bd0632e6571feb0d662
Yash Mehta <yashsmehta95@gmail.com>
2023-06-29 11:29:56 -0400

    change to trainer with cfg object input

--- plasticity/behavior/trainer.py
+++ plasticity/behavior/trainer.py
@@ -10,42 +10,26 @@ from jax.random import split
 import time
 
 
-if __name__ == "__main__":
-    num_epochs = 1000
-    num_exps = 10
-    num_blocks, trials_per_block = 3, 100
-    num_odors = 2
-    reward_ratios = ((0.2, 0.8), (0.8, 0.2), (0.2, 0.8))
-    # specify trainable parameters
-    coeff_mask = np.zeros((3, 3, 3))
-    coeff_mask[1, 1, 0] = 1
-    coeff_mask[1, 0, 0] = 1
-    coeff_mask[0, 1, 0] = 1
-    coeff_mask[0, 0, 0] = 1
+def train(cfg):
 
-    assert len(reward_ratios) == num_blocks, print(
-        "length of reward ratios should be equal to number of blocks!"
-    )
-
-    input_dim, output_dim = 2, 1
-    key = jax.random.PRNGKey(0)
-
+    coeff_mask = np.array(cfg.coeff_mask)
+    key = jax.random.PRNGKey(cfg.seed)
     simulation_coeff, plasticity_func = synapse.init_reward_volterra(init="reward")
     plasticity_coeff, _ = synapse.init_reward_volterra(init="zeros")
 
-    key, key2 = split(key)
+    key, _ = split(key)
 
     # winit = jnp.zeros((input_dim, output_dim))
-    winit = utils.generate_gaussian(key, (input_dim, output_dim), scale=0.01)
+    winit = utils.generate_gaussian(key, (cfg.input_dim, cfg.output_dim), scale=0.01)
     print(f"initial weights: \n{winit}")
-    key, key2 = split(key)
+    key, _ = split(key)
 
     mus, sigmas = inputs.generate_binary_input_parameters()
 
     # are we running on CPU or GPU?
     device = jax.lib.xla_bridge.get_backend().platform
     print("platform: ", device)
-    print("layer size: [{}, {}]".format(input_dim, output_dim))
+    print("layer size: [{}, {}]".format(cfg.input_dim, cfg.output_dim))
     print()
 
     (
@@ -56,22 +40,23 @@ if __name__ == "__main__":
         expected_rewards,
     ) = data_loader.simulate_all_experiments(
         key,
-        num_exps,
+        cfg.num_exps,
         winit,
         simulation_coeff,
         plasticity_func,
         mus,
         sigmas,
-        reward_ratios,
-        trials_per_block,
+        cfg.reward_ratios,
+        cfg.trials_per_block,
     )
 
     loss_value_and_grad = jax.value_and_grad(losses.celoss, argnums=1)
     optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(plasticity_coeff)
+    explogs = {"coeffs": [], "weights": []}
 
-    for epoch in range(num_epochs):
-        for exp_i in range(num_exps):
+    for epoch in range(cfg.num_epochs):
+        for exp_i in range(cfg.num_exps):
             start = time.time()
             # calculate the length of each trial by checking for NaNs
             trial_lengths = jnp.sum(
@@ -123,7 +108,7 @@ if __name__ == "__main__":
         if np.isnan(loss):
             print("loss is nan!")
             break
-        if epoch % 100 == 0:
+        if epoch % 25 == 0:
             print(f"epoch :{epoch}")
             print(f"loss :{loss}")
             print(
@@ -132,13 +117,14 @@ if __name__ == "__main__":
                 plasticity_coeff[0, 1, 0],
                 plasticity_coeff[0, 0, 0],
             )
+            explogs["coeffs"].append(plasticity_coeff)
             # print(
             #     plasticity_coeff
             # )
             print()
 
         # jax.debug.print("plasticity_coeff: {}", plasticity_coeff)
-
+    np.savez('explogs', **explogs)
     # save loss into file as numpy array
     # np.save("expdata/loss.npy", np.array(loss_t))
     # np.save("expdata/coeff.npy", np.array(coeff_t))
