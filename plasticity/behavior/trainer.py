5a294f70a2645c7edbcac515856d11e138d4a4af
Yash Mehta <yashsmehta95@gmail.com>
2023-09-14 15:56:27 -0400

    clean up trainer: util funcs for print, log, save

--- plasticity/behavior/trainer.py
+++ plasticity/behavior/trainer.py
@@ -1,15 +1,14 @@
 import plasticity.inputs as inputs
 from plasticity import synapse
-from plasticity.behavior.utils import coeff_logs_to_dict
 import plasticity.behavior.data_loader as data_loader
 import plasticity.behavior.losses as losses
 import plasticity.behavior.model as model
+import plasticity.behavior.utils as utils
 import jax
 import jax.numpy as jnp
 import optax
 import numpy as np
 from jax.random import split
-from pathlib import Path
 import pandas as pd
 from statistics import mean
 import time
@@ -17,8 +16,8 @@ import time
 
 def train(cfg):
     jax.config.update("jax_platform_name", "cpu")
+    utils.assert_valid_config(cfg)
     key = jax.random.PRNGKey(cfg.jobid)
-    np.random.seed(cfg.jobid)
     generation_coeff, generation_func = synapse.init_plasticity(
         key, cfg, mode="generation_model"
     )
@@ -34,7 +33,6 @@ def train(cfg):
     device = jax.lib.xla_bridge.get_backend().platform
     print("platform: ", device)
     print(f"layer size: {cfg.layer_sizes}")
-    start = time.time()
 
     if cfg.use_experimental_data:
         (
@@ -44,6 +42,7 @@ def train(cfg):
             expected_rewards,
         ) = data_loader.load_single_adi_experiment(cfg)
     else:
+        # can remove odors altogether
         (
             resampled_xs,
             odors,
@@ -63,11 +62,10 @@ def train(cfg):
     loss_value_and_grad = jax.value_and_grad(losses.celoss, argnums=1)
     optimizer = optax.adam(learning_rate=1e-3)
     opt_state = optimizer.init(plasticity_coeff)
-    coeff_logs, epoch_logs, loss_logs = [], [], []
+    expdata = {}
 
     for epoch in range(cfg.num_epochs):
         for exp_i in range(cfg.num_exps):
-            start = time.time()
             # calculate the length of each trial by checking for NaNs
             # note: this can all be calculated inside the loss function!
             trial_lengths = jnp.sum(
@@ -110,33 +108,18 @@ def train(cfg):
 
             plasticity_coeff = optax.apply_updates(plasticity_coeff, updates)
 
-        # check if loss is nan
-        if np.isnan(loss):
-            print("loss is nan!")
-            break
         if epoch % cfg.log_interval == 0:
-            print(f"epoch :{epoch}")
-            print(f"loss :{loss}")
-            if cfg.plasticity_model == "volterra":
-                indices = cfg.coeff_mask.nonzero()
-                ind_i, ind_j, ind_k = indices
-                for index in zip(ind_i, ind_j, ind_k):
-                    print(f"A{index}", plasticity_coeff[index])
-                print("\n")
-            else:
-                print("MLP plasticity coeffs: ", plasticity_coeff)
-
-            coeff_logs.append(plasticity_coeff)
-            epoch_logs.append(epoch)
-            loss_logs.append(loss)
+            expdata = utils.print_and_log_training_info(
+                cfg, expdata, plasticity_coeff, epoch, loss
+            )
 
     key, _ = split(key)
     r2_score, percent_deviance = model.evaluate(
         key,
         cfg,
         generation_coeff,
-        plasticity_coeff,
         generation_func,
+        plasticity_coeff,
         plasticity_func,
         mus,
         sigmas,
@@ -144,12 +127,7 @@ def train(cfg):
 
     print(f"r2 score: {r2_score}")
     print(f"percent deviance: {percent_deviance}")
-    exit()
 
-    coeff_logs = np.array(coeff_logs)
-    expdata = coeff_logs_to_dict(coeff_logs)
-    expdata["epoch"] = epoch_logs
-    expdata["loss"] = loss_logs
     df = pd.DataFrame.from_dict(expdata)
     df["r2_weights"], df["r2_activity"] = mean(r2_score["weights"]), mean(
         r2_score["activity"]
@@ -159,19 +137,7 @@ def train(cfg):
     for key, value in cfg.items():
         if isinstance(value, (float, int)):
             df[key] = value
-            # print(key, value)
 
     # pd.set_option("display.max_columns", None)
     print(df.tail(5))
-
-    if cfg.log_expdata:
-        logdata_path = Path(cfg.log_dir)
-        if cfg.use_experimental_data:
-            logdata_path = logdata_path / "expdata" / cfg.exp_name
-        else:
-            logdata_path = logdata_path / "simdata" / cfg.exp_name
-
-        logdata_path.mkdir(parents=True, exist_ok=True)
-        csv_file = logdata_path / f"exp_{cfg.jobid}.csv"
-        write_header = not csv_file.exists()
-        df.to_csv(csv_file, mode="a", header=write_header, index=False)
+    utils.save_logs(cfg, df)
